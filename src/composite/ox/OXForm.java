3e90fff7be3f7638b448d2de72b916cb7fde2554
BrightComposite <maestro.aquarius@gmail.com>
2016-03-21 00:13:27 +0300

    Added buttons to main menu, renamed OXForm to OxMainMenuForm

--- src/composite/ox/OXForm.java
+++
@@ -1,7 +0,0 @@
-package composite.ox;
-
-/**
- * Created by Aquarius on 20-Mar-16.
- */
-public class OXForm {
-}
---
+++ src/composite/ox/OxMainMenuForm.java
@@ -0,0 +1,13 @@
+package composite.ox;
+
+import javax.swing.*;
+
+public class OxMainMenuForm {
+    private JButton playTwoButton;
+    private JPanel panel1;
+    private JButton exitButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
